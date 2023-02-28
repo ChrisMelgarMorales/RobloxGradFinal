a79dad7f4ecef7a1e67c6b52e2096b327c28c65a
Kris Melgar Morales <cmelgarmorales@csu.fullerton.edu>
2023-02-28 00:54:17 -0800

    Added Whitelist based on Roblox ID

--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -1,1 +1,36 @@
 print("Hello world, from server!")
+-- Get a reference to the server script
+local serverScript = game:GetService("ServerScriptService"):WaitForChild("Server")
+
+-- Get a reference to the StringValue instance
+local stringValue = serverScript:FindFirstChild("whitelist")
+
+-- Check if the StringValue was found
+local whitelistUserIDs = {}
+if stringValue then
+    -- Access the value of the StringValue
+	local substrings = string.split(stringValue.Value, "\n")
+	
+	-- Convert each substring to a number and add it to an array
+	
+	for i, substring in ipairs(substrings) do
+		whitelistUserIDs[i] = tonumber(substring)
+	end
+	
+	-- Print the array of numbers
+	print(table.concat(whitelistUserIDs, ", "))
+else
+    print("Could not find the StringValue instance!")
+end
+
+
+local Players = game:GetService("Players")
+
+
+Players.PlayerAdded:Connect(function(player)
+
+	if not table.find(whitelistUserIDs,player.UserId) then --If the UserID value is not in the table this returns nil. In Lua nil equals false.
+		player:Kick("You are not whitelisted on this server.")
+	end
+
+end)
